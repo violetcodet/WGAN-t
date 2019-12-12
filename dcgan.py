245b05a910c3f93cdd34562e3d1dfbe46a25ed01
T.yp <zicaibaozicai@gmail.com>
2019-12-12 12:54:51 +0900

    Update dcgan.py

--- dcgan.py
+++ dcgan.py
@@ -78,7 +78,7 @@ for i in range(len(input_list)):
 
 (train_images, _), (_, _) = keras.datasets.fashion_mnist.load_data()
 
-train_images = train_images.reshape(train_images.shape[0], 1500, 1000, 1).astype('float32')
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
 
 train_images = (train_images - 127.5) / 127.5
 
